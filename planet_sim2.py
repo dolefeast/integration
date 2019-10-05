821a2a87e532c6ac05e52a90b1bdd90dda2ea9b6
freierdvogel <elrois456yt@gmail.com>
2019-10-05 14:07:04 +0200

    plotted orbit

--- planet_sim2.py
+++ planet_sim2.py
@@ -1,9 +1,10 @@
 import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from time import sleep
 
-
-v0 = [0, 10, 1, 0]
+v0 = [5, 10, 1, 0]
 
 def acc(v, t):
     u = v[:2]
@@ -23,7 +24,13 @@ p_path = spi.odeint(acc, v0, t)
 
 fig, ax = plt.subplots(1, 1)
 
-for i in p_path:
-    plt.plot((i[0], i[1]))
+print(fig, ax)
 
-plt.show()
+pt = np.matrix.transpose(p_path)
+ax.set_xlim(min(pt[0]), max(pt[0]))
+ax.set_ylim(min(pt[1]), max(pt[1]))
+print(pt)
+for i in range(len(pt[0])):
+    plt.scatter(pt[0][i], pt[1][i], color = 'red')
+    plt.draw()
+    plt.pause(1/60)
